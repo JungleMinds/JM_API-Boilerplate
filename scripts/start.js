fcee6ab0bfd443c65bc74ec6c91e2ca4baad007d
grumpyoldman-io <hello@grumpyoldman.io>
2018-10-09 13:34:20 +0200

    Added dependency checks

--- scripts/start.js
+++ scripts/start.js
@@ -1,3 +1,5 @@
 process.env.NODE_ENV = process.env.NODE_ENV || 'development'
 
+require('./utils/checkDependencies')
+
 module.exports = require('../index.js')
---
+++ scripts/utils/checkDependencies.js
@@ -0,0 +1,12 @@
+/* eslint-disable no-console */
+
+require('check-dependencies')({}, result => {
+  if (result.error.length) {
+    console.log('\n\u001b[31mMissing dependencies!\u001b[39m\n')
+    result.error.forEach(err => console.error(err))
+    console.log()
+    process.exit(1)
+  }
+})
+
+/* eslint-enable no-console */
