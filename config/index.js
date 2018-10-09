7974393a7e39b98a145d658a27c6cce40e2e8e1e
Ahmad Kabakibi <ahmadkabakibi@ahmad-kabakibi.local>
2018-10-09 14:03:55 +0200

    update config with es6 export and require.default

--- config/index.js
+++ config/index.js
@@ -4,7 +4,7 @@ const path = require('path')
 dotenv.load()
 const ENV = process.env.NODE_ENV || 'development'
 
-const envConfig = require(path.join(__dirname, 'environments', ENV))
+const envConfig = require(path.join(__dirname, 'environments', ENV)).default
 
 const config = Object.assign(
   {
