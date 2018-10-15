410b8c68e3aa4a5e5c57e1e20d4ac5ebd11ca6cd
Ahmad Kabakibi <ahmadkabakibi@ahmad-kabakibi.local>
2018-10-15 16:59:33 +0200

    revert deleted line from check-dependencies

--- scripts/utils/checkDependencies.js
+++ scripts/utils/checkDependencies.js
@@ -6,6 +6,7 @@ require('check-dependencies')({}, result => {
   if (result.error.length) {
     console.log('\n\u001b[31mMissing dependencies!\u001b[39m\n')
     result.error.forEach(err => console.error(err))
+    console.log()
     process.exit(1)
   }
 })
