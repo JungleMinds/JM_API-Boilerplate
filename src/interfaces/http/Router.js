4409774a416928c9a4e4fecdb4adaa34294a0aec
Ahmad Kabakibi <ahmadkabakibi@ahmad-kabakibi.local>
2018-10-15 15:41:09 +0200

    update prefix route name users instead of singular format

--- src/interfaces/http/Router.js
+++ src/interfaces/http/Router.js
@@ -38,7 +38,7 @@ export default ({
   /*
    * Add your API routes here
    */
-  apiRouter.use('/user', controller('user'))
+  apiRouter.use('/users', controller('user'))
 
   router.use('/api', apiRouter)
   router.use(errorHandler)
