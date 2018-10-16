33128e0d61213ca902c05d886e67d28ec950bb29
Ahmad Kabakibi <ahmadkabakibi@ahmad-kabakibi.local>
2018-10-16 15:33:01 +0200

    move pacakge exact to start script

--- scripts/start.js
+++ scripts/start.js
@@ -6,5 +6,8 @@ require('./utils/env')
 // Ensure dependency requirements are met.
 require('./utils/checkDependencies')
 
+//Ensure only have exact versions installed
+require('./utils/package-exact')
+
 // expose the app
 module.exports = require('../index.js')
