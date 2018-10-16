369e6e6a02e5e3c12ac9f9aa58358455a164a639
Ahmad Kabakibi <ahmadkabakibi@ahmad-kabakibi.local>
2018-10-16 10:49:18 +0200

    update naming conventions for user repository

--- src/interfaces/http/router.js
+++ src/interfaces/http/router.js
@@ -41,7 +41,7 @@ export default ({
   /*
    * Add your API routes here
    */
-  apiRouter.use('/users', controller('user'))
+  apiRouter.use('/user', controller('user'))
 
   router.use('/api', apiRouter)
   router.use(errorHandler)
