388d8d5b851819c40321c7871c69985011361a5e
grumpyoldman-io <hello@grumpyoldman.io>
2018-10-22 15:00:08 +0200

    Add EventStore to Application layer

---
+++ src/app/EventStore.js
@@ -0,0 +1,20 @@
+import EventEmitter from 'events'
+
+class EventStore extends EventEmitter {
+  constructor() {
+    super()
+    this.outputs = {}
+  }
+
+  on(output, handler) {
+    if (this.outputs[output]) {
+      return this.addListener(output, handler)
+    }
+
+    throw new Error(
+      `Invalid output '${output}' to EventStore ${this.constructor.name}.`
+    )
+  }
+}
+
+export default EventStore
