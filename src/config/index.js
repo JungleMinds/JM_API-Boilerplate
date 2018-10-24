bfe4cb1031c34f0c7bb7d362884469f264bb9d1a
Ahmad Kabakibi <ahmadkabakibi@ahmad-kabakibi.local>
2018-10-24 14:15:16 +0200

    add database config

--- src/config/index.js
+++ src/config/index.js
@@ -1,13 +1,23 @@
+import fs from 'fs'
 import path from 'path'
 
 const COMMIT_HASH = process.env.COMMIT_HASH || 'N.A.'
 
 const config = (ENV = 'development') => {
   let envConfig = {}
+  let dbConfig
 
   try {
     envConfig = require(path.join(__dirname, 'environments', ENV)).default
+    if (process.env.DATABASE_URL) {
+      dbConfig = process.env.DATABASE_URL
+    }
+
+    if (fs.existsSync(path.join(__dirname, './database.js'))) {
+      dbConfig = require('./database.js')[ENV]
+    }
   } catch (err) {
+    console.log(err)
     throw new Error(`Can't find a config for the environment '${ENV}'`)
   }
 
@@ -15,6 +25,7 @@ const config = (ENV = 'development') => {
     version: COMMIT_HASH,
     [ENV]: true,
     env: ENV,
+    db: dbConfig,
     ...envConfig
   }
 }
