25462b9f44ac97788918e5b2da24f1b609fa3da2
Ahmad Kabakibi <ahmadkabakibi@ahmad-kabakibi.local>
2018-10-29 16:13:36 +0100

    modify database config db name and pool

--- src/config/database.js
+++ src/config/database.js
@@ -2,22 +2,37 @@ module.exports = {
   development: {
     username: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
-    database: process.env.DB_DB,
+    database: process.env.DB_NAME,
     host: process.env.DB_HOST,
-    dialect: 'postgres'
+    dialect: 'postgres',
+    pool: {
+      max: 5,
+      idle: 30000,
+      acquire: 60000
+    }
   },
   test: {
     username: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
-    database: process.env.DB_DB,
+    database: process.env.DB_NAME,
     host: process.env.DB_HOST,
-    dialect: 'postgres'
+    dialect: 'postgres',
+    pool: {
+      max: 5,
+      idle: 30000,
+      acquire: 60000
+    }
   },
   production: {
     username: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
-    database: process.env.DB_DB,
+    database: process.env.DB_NAME,
     host: process.env.DB_HOST,
-    dialect: 'postgres'
+    dialect: 'postgres',
+    pool: {
+      max: 5,
+      idle: 30000,
+      acquire: 60000
+    }
   }
 }
