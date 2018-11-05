3eaaf2358dd02c0d497de077657ba19de2e6b6d5
grumpyoldman-io <hello@grumpyoldman.io>
2018-11-05 10:40:41 +0100

    Added multi-threading support to the test script

--- scripts/test.js
+++ scripts/test.js
@@ -2,6 +2,8 @@
 process.env.BABEL_ENV = 'test'
 process.env.NODE_ENV = 'test'
 
+const chalk = require('chalk')
+
 // Ensure environment variables are read.
 require('./utils/env')
 
@@ -14,5 +16,15 @@ let argv = process.argv.slice(2)
 // Set path to config
 argv.push('-c')
 argv.push('jest.config.json')
+
+// Set the max workers to the number of available cores minus 1, with a min. of 1
+const cpus = require('os').cpus().length || 1
+console.info(
+  chalk.green('Running tests on'),
+  chalk.bold.white(cpus),
+  chalk.green('workers'),
+  `\n`
+)
+argv.push(`--maxWorkers=${cpus}`)
 
 jest.run(argv)
