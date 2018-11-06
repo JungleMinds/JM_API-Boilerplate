bb6295364af92ce9982aa68f67110f6cedc71911
grumpyoldman-io <hello@grumpyoldman.io>
2018-11-06 16:44:08 +0100

    Added a test for preventing Work In Progress commits!

--- scripts/utils/checkWipCommits.js
+++ scripts/utils/checkWipCommits.js
@@ -1,31 +1,40 @@
 //Ensure package.json dependencies only have exact versions installed
+const chalk = require('chalk')
 const exec = require('child_process').exec
-const assert = require('assert')
 
-const handleGit = () =>
-  new Promise((resolve, reject) => {
-    exec('git log --first-parent', (err, stdout, stderror) => {
-      if (!err && !stderror) {
-        resolve(stdout.split('commit').filter(Boolean))
-      } else {
-        if (stderror) {
-          return reject(new Error(stderror))
-        }
-        return reject(new Error(stderror))
-      }
-    })
-  })
-
-const checkCommits = async () => {
-  let commits = []
-  try {
-    commits = await handleGit()
-  } catch (err) {
-    throw err
-  }
-  commits.forEach(commit => {
-    assert(!commit.includes('WIP'), `Commit: "${commit}" should not be pushed!`)
-  })
+const exitWithError = err => {
+  console.error(err.message)
+  process.exit(1)
 }
 
-checkCommits()
+exec('git log --first-parent', (err, stdout, stderror) => {
+  if (!err && !stderror) {
+    const commits = stdout
+      .split('commit')
+      .filter(Boolean)
+      .map(commit => `commit${commit}`)
+    commits.forEach(commit => {
+      if (
+        commit.includes('WIP') ||
+        commit.toLowerCase().includes('[wip]') ||
+        commit.toLowerCase().includes('--wip--')
+      ) {
+        exitWithError(
+          new Error(
+            `${chalk.bold.red(
+              'the following commit appears to be a WIP commit:'
+            )}\n\n${chalk.white(commit)}${chalk.red(
+              'Please resolve this commit before pushing to master'
+            )}\n`
+          )
+        )
+      }
+    })
+  } else {
+    if (stderror) {
+      exitWithError(new Error(stderror))
+    } else {
+      exitWithError(err)
+    }
+  }
+})
