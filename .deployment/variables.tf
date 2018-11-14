ae2c63e14621d8ab668d0c38a14db680361c647c
Ahmad Kabakibi <ahmadkbakibi@gmail.com>
2018-11-14 15:23:01 +0100

    add terraform cloudformation config

--- .deployment/variables.tf
+++ .deployment/variables.tf
@@ -1,11 +1,11 @@
-variable "aws_access_key" {
-  type = "string"
-}
-
-variable "aws_secret_key" {
-  type = "string"
-}
+variable "aws_access_key" {}
+variable "aws_secret_key" {}
+variable "aws_account_id" {}
 
 variable "aws_region" {
-  type = "string"
+  description = "AWS region e.g. eu-central-1 (Please specify a region supported by the Fargate launch type)"
+}
+
+variable "aws_resource_prefix" {
+  description = "Prefix to be used in the naming of some of the created AWS resources e.g. JM_"
 }
